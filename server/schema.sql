4e1ff79f968b230204eef8bb60fb75e845b9e924
Zynh Ludwig <Zynh0722@gmail.com>
2022-03-16 15:48:10 -0700

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,14 +1,80 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
+-- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'messages'
+--
+-- ---
+
+DROP TABLE IF EXISTS `messages`;
+
+CREATE TABLE `messages` (
+  `id` INTEGER NOT NULL,
+  `content` TEXT NULL DEFAULT NULL,
+  `room` INTEGER NULL DEFAULT NULL,
+  `author` TEXT NULL DEFAULT NULL,
+  `github_handle` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
 
-/* Create other tables and define schemas for them here! */
+-- ---
+-- Table 'rooms'
+--
+-- ---
 
+DROP TABLE IF EXISTS `rooms`;
 
+CREATE TABLE `rooms` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Table 'github_handles'
+--
+-- ---
+
+DROP TABLE IF EXISTS `github_handles`;
+
+CREATE TABLE `github_handles` (
+  `id` INTEGER NOT NULL AUTO_INCREMENT,
+  `name` TEXT NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `messages` ADD FOREIGN KEY (room) REFERENCES `rooms` (`id`);
+ALTER TABLE `messages` ADD FOREIGN KEY (github_handle) REFERENCES `github_handles` (`id`);
+
+-- ---
+-- Table Properties
+-- ---​
+-- ALTER TABLE `messages` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `rooms` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `github_handles` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `messages` (`id`,`content`,`room`,`author`,`github_handle`) VALUES
+-- ('','','','','');
+-- INSERT INTO `rooms` (`id`,`name`) VALUES
+-- ('','');
+-- INSERT INTO `github_handles` (`id`,`name`) VALUES
+-- ('','');
 
 
 /*  Execute this file from the command line by typing:
